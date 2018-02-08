c3800f03afc0a7e2048a9e5da4c78357324d9dbf
unknown <mkap25@yahoo.com>
2018-02-07 19:42:12 -0500

    first push

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+use burgers_db;
+
+  create table burgers_tbl(
+    id int primary key auto_increment,
+    burger_name varchar(30) not null,
+    devoured boolean,
+    burger_time timestamp
+  );
+  
+  
